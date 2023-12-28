550076ca166876b88aaf474361290591c4f7520a
Aris Lacdao <aceraven777@gmail.com>
2023-12-28 20:20:47 +0800

    Episode 39 - The Compile Script

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.17;
 contract Inbox {
     string public message;
 
-    function Inbox(string InitialMessage) public {
+    constructor(string InitialMessage) public {
         message = InitialMessage;
     }
 

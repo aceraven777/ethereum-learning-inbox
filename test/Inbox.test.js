24f2ba9fa1e0ff74ec8838c8ba9e9088870ab4f6
Aris Lacdao <aceraven777@gmail.com>
2023-12-29 10:54:24 +0800

    Episode 47 - Fetching Accounts from Ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,28 +3,20 @@ const ganache = require('ganache');
 const { Web3 } = require('web3');
 const web3 = new Web3(ganache.provider());
 
-class Car {
-    park() {
-        return 'stopped';
-    }
-
-    drive() {
-        return 'vroom';
-    }
-}
-
-let car;
-
 beforeEach(() => {
-    car = new Car();
+    // Get a list of all accounts
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
+
+    // Use one of those accounts to deploy
+    // the contract
+    
 });
 
-describe('Car', () => {
-    it('can park', () => {
-        assert.equal(car.park(), 'stopped');
-    });
+describe('Inbox', () => {
+    it('deploys a contract', () => {
 
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom');
     });
 });

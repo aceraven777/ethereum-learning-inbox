f768f51b00b09bbfb48f29cd24110db5ea81f8d2
Aris Lacdao <aceraven777@gmail.com>
2023-12-29 19:43:47 +0800

    Episode 58 - Deployment to Testnet

--- deploy.js
+++ deploy.js
@@ -10,3 +10,26 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+    // Get a list of all accounts
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+    
+    // Use one of those accounts to deploy
+    // the contract
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode,
+            arguments: ['Hi there!'],
+        })
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+};
+deploy();
